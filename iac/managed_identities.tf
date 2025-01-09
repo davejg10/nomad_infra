f9611b466292a23e6599690edfcb08de59ed1c2a
David <davejohngoddard@gmail.com>
2025-01-09 15:40:30 +0000

    [NOM-32] - Fine-graining the permissions

--- iac/managed_identities.tf
+++ iac/managed_identities.tf
@@ -5,7 +5,7 @@ resource "azurerm_user_assigned_identity" "asp" {
   location            = var.environment_settings.region
 }
 
-resource "azurerm_role_assignment" "webapp_fetch_secret" {
+resource "azurerm_role_assignment" "web_app_fetch_secret" {
   scope                = azurerm_key_vault.nomad.id
   role_definition_name = "Key Vault Secrets User"
   principal_id         = azurerm_user_assigned_identity.asp.principal_id
@@ -25,18 +25,75 @@ resource "azurerm_user_assigned_identity" "github" {
   location            = var.environment_settings.region
 }
 
-resource "azurerm_role_assignment" "github_to_key_Vault" {
-  scope                = data.azurerm_resource_group.rg.id
-  role_definition_name = "Contributor"
+resource "azurerm_role_assignment" "github_to_key_vault" {
+  scope                = azurerm_key_vault.nomad.id
+  role_definition_id = azurerm_role_definition.manage_key_vault_secrets.role_definition_resource_id
   principal_id         = azurerm_user_assigned_identity.github.principal_id
 }
-# resource "azurerm_role_assignment" "github_to_key_Vault" {
-#   scope                = azurerm_key_vault.nomad.id
-#   role_definition_name = "Key Vault Secrets Officer"
-#   principal_id         = azurerm_user_assigned_identity.github.principal_id
-# }
-# resource "azurerm_role_assignment" "github_to_key_Vault" {
-#   scope                = azurerm_key_vault.nomad.id
-#   role_definition_name = "Key Vault Secrets Officer"
-#   principal_id         = azurerm_user_assigned_identity.github.principal_id
-# }
+resource "azurerm_role_assignment" "github_to_acr" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_id = azurerm_role_definition.push_acr_image.role_definition_resource_id
+  principal_id         = azurerm_user_assigned_identity.github.principal_id
+}
+resource "azurerm_role_assignment" "github_to_web_app" {
+  scope                = azurerm_linux_web_app.web_app.id
+  role_definition_id = azurerm_role_definition.deploy_web_app_image.role_definition_resource_id
+  principal_id         = azurerm_user_assigned_identity.github.principal_id
+}
+
+resource "azurerm_role_definition" "manage_key_vault_secrets" {
+  name        = "${var.environment_settings.environment}-github-helper-${var.environment_settings.app_name}"
+  scope       = azurerm_key_vault.nomad.id
+  description = "A custom role tallowing all secret management in Key Vault: ${azurerm_key_vault.nomad.id.name}."
+
+  permissions {
+    actions     = []
+    data_actions = [
+      "Microsoft.KeyVault/vaults/secrets/*",
+    ]
+    not_actions = []
+  }
+
+  assignable_scopes = [
+    azurerm_key_vault.nomad.id
+  ]
+}
+
+resource "azurerm_role_definition" "push_acr_image" {
+  name        = "${var.environment_settings.environment}-acrpush-${azurerm_container_registry.acr.name}"
+  scope       = azurerm_container_registry.acr.id
+  description = "A custom role allow you to push images to the ACR: ${azurerm_container_registry.acr.name}."
+
+  permissions {
+    actions: [
+        "Microsoft.ContainerRegistry/registries/pull/read",
+        "Microsoft.ContainerRegistry/registries/push/write"
+    ],
+    data_actions = []
+    not_actions = []
+  }
+
+  assignable_scopes = [
+    azurerm_container_registry.acr.id
+  ]
+}
+
+resource "azurerm_role_definition" "deploy_web_app_image" {
+  name        = "${var.environment_settings.environment}-webapp-deploy-${aazurerm_linux_web_app.web_app.name}"
+  scope       = azurerm_linux_web_app.web_app.id
+  description = "A custom role allow you to fetch publish_profile and deploy Web Apps to: ${azurerm_linux_web_app.web_app.name}."
+
+  permissions {
+    actions: [
+      "Microsoft.Web/sites/containers/*"
+    ],
+    data_actions = [
+      "Microsoft.Web/sites/publishingProfiles/list/action"
+    ]
+    not_actions = []
+  }
+
+  assignable_scopes = [
+    azurerm_linux_web_app.web_app.id
+  ]
+}

24672d0d9b5d94ffd836018155bd73ff77f027d8
David <davejohngoddard@gmail.com>
2025-02-11 13:46:42 +0000

    d

--- iac/scripts/configure_vm.sh
+++ iac/scripts/configure_vm.sh
@@ -9,14 +9,15 @@ NEO4J_DATA_DIR="/datadisk"
 # ===============Mount the drive=================
 # Programatiicaly find datadisk name by suggesting size and mountpoint empty
 DATADISK=$(lsblk -d -n -o NAME,SIZE,MOUNTPOINT | awk -v size="${neo4j_data_disk_size_gb}G" '$2 == size  && $3 == "" {print $1}' | head -1)
-
-sudo mkdir $NEO4J_DATA_DIR
-# Format disk
-sudo mkfs.ext4 /dev/$DATADISK
-# Mount disk
-sudo mount /dev/$DATADISK $NEO4J_DATA_DIR
-# Add to fstab for persistence
-echo "/dev/$DATADISK $NEO4J_DATA_DIR ext4 defaults 0 0" | sudo tee -a /etc/fstab
+if [ -n "$DATADISK" ]; then
+    sudo mkdir $NEO4J_DATA_DIR
+    # Format disk
+    sudo mkfs.ext4 /dev/$DATADISK
+    # Mount disk
+    sudo mount /dev/$DATADISK $NEO4J_DATA_DIR
+    # Add to fstab for persistence
+    echo "/dev/$DATADISK $NEO4J_DATA_DIR ext4 defaults 0 0" | sudo tee -a /etc/fstab
+fi
 
 # Attempt to get Neo4j version
 neo4j_version=$(neo4j --version 2>/dev/null)

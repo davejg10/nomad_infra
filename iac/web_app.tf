492b828eb69d5f33b388b4bdffabe5ff9621a00c
David <davejohngoddard@gmail.com>
2025-02-12 12:52:01 +0000

    Setting connection string environment variable in web app

--- iac/web_app.tf
+++ iac/web_app.tf
@@ -39,8 +39,13 @@ resource "azurerm_linux_web_app" "web_app" {
     application_logs {
       file_system_level = "Information"
     }
+    http_logs { 
+      file_system {
+        retention_in_days = 7
+        retention_in_mb = 35
+      }
+    }
   }
-  
 
   lifecycle {
     ignore_changes = [

e343016fc8cd842ca51df5b480176ad8e72832be
David <davejohngoddard@gmail.com>
2025-02-25 17:53:59 +0000

    Pulled main

--- iac/data-services/network.tf
+++ iac/data-services/network.tf
@@ -1,4 +1,5 @@
+# The subnet is created in ../backend Terraform config directory
 resource "azurerm_app_service_virtual_network_swift_connection" "vnet_integration" {
-  app_service_id = azurerm_linux_web_app.producer.id
-  subnet_id      = data.terraform_remote_state.devopsutils.outputs.data_services_subnet_id
+  app_service_id = azurerm_linux_function_app.producer.id
+  subnet_id      = data.terraform_remote_state.backend.outputs.data_services_subnet_id
 }

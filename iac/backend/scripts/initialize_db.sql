da104069751b7283122cf7c5b0e037ca8d17fd44
David <davejohngoddard@gmail.com>
2025-03-01 19:17:35 +0000

    Added print statement

--- iac/backend/scripts/initialize_db.sql
+++ iac/backend/scripts/initialize_db.sql
@@ -7,18 +7,18 @@ CREATE DATABASE nomad;
 SELECT :'NOMAD_ADMIN_USER';
 
 -- These are the managed identity we have created
-select * from pgaadauth_create_principal(:'NOMAD_ADMIN_USER', false, false);
-select * from pgaadauth_create_principal(:'NOMAD_BACKEND_USER', false, false);
+select * from pgaadauth_create_principal(:"NOMAD_ADMIN_USER", false, false);
+select * from pgaadauth_create_principal(:"NOMAD_BACKEND_USER", false, false);
 
 CREATE ROLE nomad_admin;
 CREATE ROLE nomad_backend;
 CREATE ROLE nomad_function_app; -- We will assign the function app identity to this later when its created
 
-GRANT nomad_admin TO :NOMAD_ADMIN_USER;
-GRANT nomad_backend TO :NOMAD_BACKEND_USER;
+GRANT nomad_admin TO :"NOMAD_ADMIN_USER";
+GRANT nomad_backend TO :"NOMAD_BACKEND_USER";
 
 GRANT ALL PRIVILEGES ON DATABASE nomad TO nomad_admin;
 
-ALTER DATABASE nomad OWNER TO :NOMAD_ADMIN_USER;
+ALTER DATABASE nomad OWNER TO :"NOMAD_ADMIN_USER";
 
 ALTER ROLE nomad_admin WITH CREATEROLE;
